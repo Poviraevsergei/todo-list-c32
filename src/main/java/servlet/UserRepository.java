0099fada6fe9ee48cdb8bd383a2e6b0e3ca13420
poviraevsergei <Siarhey.Pavirayeu@ventionteams.com>
2025-01-14 21:01:41 +0300

    lesson 26: html login page and LoginServlet added.

---
+++ src/main/java/servlet/UserRepository.java
@@ -0,0 +1,23 @@
+package servlet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRepository {
+    private static final Map<String, String> users = new HashMap<>();
+
+    static {
+        users.put("admin", "admin");
+        users.put("user", "user");
+    }
+
+    public static Boolean isValid(String username, String password) {
+        if (username == null || password == null) {
+            return false;
+        }
+        if (users.containsKey(username)) {
+            return users.get(username).equals(password);
+        }
+        return false;
+    }
+}
